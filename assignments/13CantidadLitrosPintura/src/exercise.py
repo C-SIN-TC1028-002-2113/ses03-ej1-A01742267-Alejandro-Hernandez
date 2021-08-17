07d472e8c3c8c3172aa4651fdff1f3e4a4fd56d4
A01742267-Alejandro-Hernandez <A01742267@itesm.mx>
2021-08-17 14:32:20 +0000

    Ejercicio2 CantidadPintura

--- assignments/13CantidadLitrosPintura/src/exercise.py
+++ assignments/13CantidadLitrosPintura/src/exercise.py
@@ -1,10 +1,10 @@
 import math
 def main():
     #escribe tu código abajo de esta línea
-    a=float(input("Area a pintar en metros:"))
-    r=float(input("Rendimiento (m2/l):"))
+    a=float(input("Area a pintar en metros: "))
+    r=float(input("Rendimiento (m2/l): "))
     l=a/r
-    print("Litros a comprar:",round(l))
+    print("Litros a comprar: ",round(l))
 
 if __name__ == '__main__':
     main()
