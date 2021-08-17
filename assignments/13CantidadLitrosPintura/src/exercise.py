1e05737be43f99da510dca7c55e131ea97529c78
A01742267-Alejandro-Hernandez <A01742267@itesm.mx>
2021-08-17 14:35:19 +0000

    Ejercicios CantidadPintura|PEMDAS

--- assignments/13CantidadLitrosPintura/src/exercise.py
+++ assignments/13CantidadLitrosPintura/src/exercise.py
@@ -1,10 +1,10 @@
 import math
 def main():
     #escribe tu código abajo de esta línea
-    a=float(input("Area a pintar en metros: "))
-    r=float(input("Rendimiento (m2/l): "))
-    l=a/r
-    print("Litros a comprar: ",round(l))
+    area=float(input("Area a pintar en metros:"))
+    rendimiento=float(input("Rendimiento (m2/l):"))
+    litros=area/rendimiento
+    print("Litros a comprar:",round(litros))
 
 if __name__ == '__main__':
     main()
